9616af89e0a38cca72e371df110b75e1b98d71ec
shayne243 <marques0@purdue.edu>
2021-01-06 11:53:39 -0500

    Added Shader for Portal Door

---
+++ Assets/DataFiles/Scripts/CameraMovement.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMovement: MonoBehaviour
+{
+    public int speed;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float moveCam = speed * Input.GetAxis("Vertical") * Time.deltaTime;
+        transform.Translate(0, 0, moveCam);
+
+        float rotateCam = 0;
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            --rotateCam;
+        }
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            ++rotateCam;
+        }
+        transform.Rotate(0, rotateCam, 0);
+    }
+}
---
+++ Assets/DataFiles/Shaders/PortalDoorShader.shader
@@ -0,0 +1,17 @@
+﻿Shader "Unlit/PortalDoorShader"
+{
+    SubShader
+    {
+        Zwrite off
+        ColorMask 0
+        Stencil 
+        {
+            Ref 1
+            Pass replace
+        }
+        Pass
+        {
+         
+        }
+    }
+}
