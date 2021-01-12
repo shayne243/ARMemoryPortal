b65192c70901a1c5a7518d8861347e8d68ef6e8a
shayne243 <marques0@purdue.edu>
2021-01-12 14:17:20 -0500

    Added Portal door script

---
+++ Assets/DataFiles/Scripts/PortalDoor.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Rendering;
+
+public class PortalDoor : MonoBehaviour
+{
+    public Material[] PGMat;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerStay(Collider collide)
+    {
+        if(collide.gameObject.CompareTag("MainCamera"))
+        {
+            if(transform.position.z > collide.transform.position.z)
+            {
+                foreach(Material mat in PGMat)
+                {
+                    mat.SetInt("stest", (int)CompareFunction.Equal);
+                }
+            }
+            else
+            {
+                foreach(Material mat in PGMat)
+                {
+                    mat.SetInt("stest", (int)CompareFunction.NotEqual);
+                }
+            }
+        }
+    }
+}
